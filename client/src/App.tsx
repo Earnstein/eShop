38ee0ff85c45182fa11dc1dd5db9e6e20e14a4f7
earnstein <bdamilola00@gmail.com>
2024-07-15 21:02:49 +0100

    feat: added Homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
-
-function App() {
-
+import Header from "@/components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "@/components/Footer";
+import HomePage from "./pages/HomePage";
+const App = () => {
   return (
-    <p>hello</p>
-  )
-}
+    <>
+      <Header />
+      <main>
+        <Container>
+          <HomePage/>
+        </Container>
+      </main>
+      <Footer/>
+    </>
+  );
+};
 
-export default App
+export default App;
