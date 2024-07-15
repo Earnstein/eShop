b14f7a3a45a63754b31ac8149328509f5a53268c
earnstein <bdamilola00@gmail.com>
2024-07-15 22:38:33 +0100

    add: index file for all assets import

---
+++ client/src/assets/images/index.ts
@@ -0,0 +1,16 @@
+import alexaImg from "@/assets/images/alexa.jpg";
+import cameraImg from "@/assets/images/camera.jpg";
+import airpodImg from "@/assets/images/airpods.jpg";
+import mouseImg from "@/assets/images/mouse.jpg";
+import playstationImg from "@/assets/images/playstation.jpg";
+import phoneImg from "@/assets/images/phone.jpg";
+
+
+export {
+    alexaImg,
+    cameraImg,
+    airpodImg,
+    mouseImg,
+    playstationImg,
+    phoneImg
+}
