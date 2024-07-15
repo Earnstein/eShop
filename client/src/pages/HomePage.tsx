3b88dec5f1d6454922d1c5441fcd7a664c8654e3
earnstein <bdamilola00@gmail.com>
2024-07-15 22:40:27 +0100

    chore: added fragment

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,20 +1,21 @@
+import React from "react";
 import products from "@/constants/products";
-import {Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import ProductCard from "@/components/ProductCard";
 
 const HomePage = () => {
   return (
-    <>
-        <h1 className="text-center py-3">Latest Product</h1>
-       <Row>
-       {products.map((product)=>(
-            <Col sm={12} md={6} lg={4} xl={3}>
-                <ProductCard {...product}/>
-            </Col>
+    <React.Fragment>
+      <h1 className="text-center py-3">Latest Product</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <ProductCard {...product} />
+          </Col>
         ))}
-       </Row>
-    </>
-  )
-}
+      </Row>
+    </React.Fragment>
+  );
+};
 
-export default HomePage
+export default HomePage;
