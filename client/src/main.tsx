0b3e7e08e2dbfe05906f4318eff2efa2d7ddd91e
earnstein <bdamilola00@gmail.com>
2024-07-15 22:38:12 +0100

    feat: implemented routing with react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import "@/assets/custom/bootstrap.custom.css";
+import { HomePage, ProductPage } from "@/pages/index.ts"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom"
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<HomePage />} index={true} />
+      <Route path="/product/:id" element={<ProductPage />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
