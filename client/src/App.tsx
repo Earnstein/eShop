d691dc92990e0eee5e4c228e4a9d96063ac11494
earnstein <bdamilola00@gmail.com>
2024-07-15 22:37:35 +0100

    chore: added outlet from react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,20 @@
+import React from "react";
 import Header from "@/components/Header";
 import { Container } from "react-bootstrap";
 import Footer from "@/components/Footer";
-import HomePage from "./pages/HomePage";
+import { Outlet } from "react-router-dom";
+
 const App = () => {
   return (
-    <>
+    <React.Fragment>
       <Header />
       <main>
         <Container>
-          <HomePage/>
+          <Outlet />
         </Container>
       </main>
-      <Footer/>
-    </>
+      <Footer />
+    </React.Fragment>
   );
 };
 
