723a1383c46bff2b0ecf07939ab802cda312e4c3
earnstein <bdamilola00@gmail.com>
2024-07-16 23:31:11 +0100

    feat: server entry

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import app from "./src/app";
+import 'colorts/lib/string'
+
+const PORT = Bun.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`.green.underline);
+})
