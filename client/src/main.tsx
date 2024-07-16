e31681b09baa1d6b183b0ce79a11ec84b9448c36
earnstein <bdamilola00@gmail.com>
2024-07-16 23:28:01 +0100

    feat: set up tanstack

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,7 +9,15 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from "react-router-dom"
+} from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,6 +30,9 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
