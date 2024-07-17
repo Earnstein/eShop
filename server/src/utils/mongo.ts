ec431fecdebcd0035d238342afa7b62629a6abb3
earnstein <bdamilola00@gmail.com>
2024-07-17 23:27:11 +0100

    add: mongo connection utils

---
+++ server/src/utils/mongo.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import 'colorts/lib/string'
+
+const MONGO_URI = Bun.env.MONGO_URI!;
+
+
+mongoose.connection.on('error', (err) => {
+    console.error(`ERROR: ${err}`.red.inverse);
+});
+
+export async function mongoConnect() {
+   try {
+    await mongoose.connect(MONGO_URI);
+    console.log('MongoDB connected'.blue.underline);
+   } catch (error) {
+    console.error(`MongoDB connection error: ${error}`.red.inverse);
+   }
+}
+
+export async function mongoDisconnect() {
+    await mongoose.connection.close();
+    console.log('MongoDB disconnected'.red.underline);
+}
