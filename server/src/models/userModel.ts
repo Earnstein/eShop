022e6cace93a081acb6dc3a573443adc984aa474
earnstein <bdamilola00@gmail.com>
2024-07-17 23:26:49 +0100

    feat: user model

---
+++ server/src/models/userModel.ts
@@ -0,0 +1,38 @@
+import { Schema, model, Document } from "mongoose";
+
+interface User extends Document {
+  name: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+}
+
+const UserSchema = new Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const User = model<User>("User", UserSchema);
+export default User;
