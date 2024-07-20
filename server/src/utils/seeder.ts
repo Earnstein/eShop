2b84e6fc7e782eb3503be7e6a328448237701f03
earnstein <bdamilola00@gmail.com>
2024-07-20 19:05:47 +0100

    add: seeder file

---
+++ server/src/utils/seeder.ts
@@ -0,0 +1,40 @@
+import { products, users } from "../constants";
+import Product from "../models/productModel";
+import Order from "../models/orderModel";
+import User from "../models/userModel";
+
+
+export const importData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const seedUsers = await User.insertMany(users);
+    const adminUser = seedUsers[0]._id;
+    const seedProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+    await Product.insertMany(seedProducts);
+    console.log(`Data seeded successfully`.green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`Error seeding file: ${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+export const deleteData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+    console.log(`Data removed successfully`.green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`Error removing seeded data: ${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
