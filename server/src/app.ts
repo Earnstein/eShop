c26644823029b473f98008c17d3a4becb48f7129
earnstein <bdamilola00@gmail.com>
2024-07-21 14:02:14 +0100

    chore: added rate limiter and cookie parser

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,18 +3,33 @@ import morgan from "morgan";
 import appRouter from "./routers";
 import cors from "cors";
 import helmet from "helmet";
+import { errorHandlerMiddleware } from './middlewares/errorHandler';
+import { rateLimit } from "express-rate-limit";
+import cookieParser from "cookie-parser";
+
+const limit = rateLimit({
+    windowMs: 0.4 * 60 * 1000,
+    max: 4,
+});
+
 
 const app = express();
 
-app.use(helmet())
 app.use(express.json());
+app.use(helmet())
 app.use(
     cors({
         origin: "http://localhost:5173",
         credentials: true,
     })
     );
-app.use(morgan("combined"));
+app.use(limit);
+app.use(cookieParser("secret"));
+if (Bun.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 app.use("/api/v1", appRouter);
+
+app.use(errorHandlerMiddleware);
 
 export default app;
