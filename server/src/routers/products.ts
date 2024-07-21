ea84a5da0942136dbccf4bf01e0114d21d467f06
earnstein <bdamilola00@gmail.com>
2024-07-21 14:03:40 +0100

    feat: added id validation midddleware

--- server/src/routers/products.ts
+++ server/src/routers/products.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { getProducts, getProduct } from '../controllers/products';
+import { paramValidator, validateId } from "../middlewares/validators";
+
 
 const productRouter = express.Router();
+
 productRouter.get("/", getProducts);
-productRouter.get("/:id", getProduct);
+productRouter.get("/:id", paramValidator, validateId, getProduct);
+
 
 export default productRouter;
