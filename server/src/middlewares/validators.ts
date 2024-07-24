750241d102698a2002d729e5509d528a85f95afd
earnstein <bdamilola00@gmail.com>
2024-07-24 16:02:07 +0100

    chore: formatted file

--- server/src/middlewares/validators.ts
+++ server/src/middlewares/validators.ts
@@ -1,38 +1,40 @@
-import { body, param, validationResult, type ValidationChain } from "express-validator";
 import type { Request, Response, NextFunction } from "express";
-import { BadRequest } from '../utils/errors';
-import { StatusCodes } from 'http-status-codes';
-
-
+import { BadRequest } from "../utils/errors";
+import { StatusCodes } from "http-status-codes";
+import {
+  body,
+  param,
+  validationResult,
+  type ValidationChain,
+} from "express-validator";
 
 export const validate = (validations: ValidationChain[]) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-       for (const validation of validations) {
-           const result = await validation.run(req);
-           if (!result.isEmpty()) {
-                break
-           }
-       }
-
-       const errors = validationResult(req);
-       if (!errors.isEmpty()) {
-           return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ errors: errors.array() });
-       }
-       return next();
+  return async (req: Request, res: Response, next: NextFunction) => {
+    for (const validation of validations) {
+      const result = await validation.run(req);
+      if (!result.isEmpty()) {
+        break;
+      }
     }
-}
 
-
-
-export const validateId =  (req:Request, res:Response, next:NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        throw new BadRequest(errors.array()[0].msg)
+      return res
+        .status(StatusCodes.UNPROCESSABLE_ENTITY)
+        .json({ errors: errors.array() });
     }
-    next();
-}
+    return next();
+  };
+};
 
+export const validateId = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new BadRequest(errors.array()[0].msg);
+  }
+  return next();
+};
 
 export const paramValidator = [
-    param("id").isMongoId().withMessage("Invalid ID"),
-]
+  param("id").isMongoId().withMessage("Invalid ID"),
+];
