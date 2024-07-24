ab7e392ac0d1dae84ff5e5d49af68e2b8ad1e83d
earnstein <bdamilola00@gmail.com>
2024-07-24 16:01:44 +0100

    feat: added async handler

--- server/src/controllers/products.ts
+++ server/src/controllers/products.ts
@@ -1,30 +1,50 @@
 import type { Request, Response } from "express";
 import Product from "../models/productModel";
 import { StatusCodes } from "http-status-codes";
+import { NotFound } from "../utils/errors";
+import asyncHandler from "express-async-handler";
 
-export async function getProducts(req: Request, res: Response) {
-  const products = await Product.find(
-    {},
-    {
-      __v: 0,
-      updatedAt: 0,
-      createdAt: 0,
+export const getProducts = asyncHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const products = await Product.find(
+      {},
+      {
+        __v: 0,
+        updatedAt: 0,
+        createdAt: 0,
+      },
+    );
+    if (products.length === 0) {
+      res.status(StatusCodes.OK).json({
+        data: 0,
+        message: "There is no product in this inventory",
+        error: "",
+      });
+      return;
     }
-  );
-  return res.status(StatusCodes.OK).json(products);
-}
+    res.status(StatusCodes.OK).json(products);
+    return;
+  },
+);
 
-export async function getProduct(req: Request, res: Response) {
-  const { id } = req.params;
-  const product = await Product.findById(
-    {
-      _id: id,
-    },
-    {
-      __v: 0,
-      updatedAt: 0,
-      createdAt: 0,
+export const getProduct = asyncHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const { id } = req.params;
+    const product = await Product.findById(
+      {
+        _id: id,
+      },
+      {
+        __v: 0,
+        updatedAt: 0,
+        createdAt: 0,
+      },
+    );
+
+    if (!product) {
+      throw new NotFound("product not found");
     }
-  );
-  return res.status(StatusCodes.OK).json(product);
-}
+    res.status(StatusCodes.OK).json(product);
+    return;
+  },
+);
