cf5bcce2c9b3a7a2eee1b673ba4068b8255c8aa1
earnstein <bdamilola00@gmail.com>
2024-07-24 15:58:42 +0100

    chore: formatted file

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,30 +3,29 @@ import morgan from "morgan";
 import appRouter from "./routers";
 import cors from "cors";
 import helmet from "helmet";
-import { errorHandlerMiddleware } from './middlewares/errorHandler';
+import { errorHandlerMiddleware } from "./middlewares/errorHandler";
 import { rateLimit } from "express-rate-limit";
 import cookieParser from "cookie-parser";
 
 const limit = rateLimit({
-    windowMs: 0.4 * 60 * 1000,
-    max: 4,
+  windowMs: 0.4 * 60 * 1000,
+  max: 4,
 });
-
 
 const app = express();
 
 app.use(express.json());
-app.use(helmet())
+app.use(helmet());
 app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-    );
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(limit);
 app.use(cookieParser("secret"));
 if (Bun.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
 app.use("/api/v1", appRouter);
 
