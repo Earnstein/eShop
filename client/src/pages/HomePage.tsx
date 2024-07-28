d1b800a716447c54bd9ac2824d21a80279cb854d
earnstein <bdamilola00@gmail.com>
2024-07-28 01:58:35 +0100

    feat: added prefetch feature to prefetch product

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -3,16 +3,28 @@ import { Row, Col } from "react-bootstrap";
 import ProductCard, { ProductProp } from "@/components/ProductCard";
 import { useGetProducts } from '../apis/api-hooks';
 import Loader from '@/components/Loader';
+import { getProduct } from "@/apis/api";
+import { useQueryClient } from "@tanstack/react-query";
+import { STALETIME } from "@/constants";
 
 const HomePage = () => {
-  const { data:products, isLoading } = useGetProducts();
+  const queryClient = useQueryClient();
+  const { data:products, isLoading, error, isError } = useGetProducts();
+  const prefetch = (id:string) => {
+    queryClient.prefetchQuery({
+      queryKey: ["product", id],
+      queryFn:  () => getProduct(id),
+      staleTime: STALETIME,
+    })
+  };
   return (
     <React.Fragment>
       <h1 className="text-center py-3">Latest Product</h1>
       <Row>
         {isLoading && <Loader/>}
+        {isError && error?.message && <p>{error?.message}</p>}
         {!isLoading && products.map((product: ProductProp) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3} onMouseEnter={()=>prefetch(product._id)} onFocus={()=>prefetch(product._id)}>
             <ProductCard {...product} />
           </Col>
         ))}
