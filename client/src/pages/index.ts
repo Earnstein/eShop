ec6d49d36b5f57184e918a69b35086edfc2c3c5f
earnstein <bdamilola00@gmail.com>
2024-07-28 21:53:14 +0100

    chore: added cart page import

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,8 +1,10 @@
 import HomePage from "@/pages/HomePage";
 import ProductPage from "@/pages/ProductPage";
+import CartPage from "@/pages/CartPage";
 
 
 export {
     HomePage,
-    ProductPage
+    ProductPage,
+    CartPage,
 }
