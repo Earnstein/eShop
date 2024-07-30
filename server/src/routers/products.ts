046e508d188bd3f587585dc06b6f105acd5d5460
earnstein <bdamilola00@gmail.com>
2024-07-30 17:59:53 +0100

    chore: added protected middlware

--- server/src/routers/products.ts
+++ server/src/routers/products.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { getProducts, getProduct } from "../controllers/products";
 import { paramValidator, validateId } from "../middlewares/validators";
+import { protectedRouteMiddleware } from '../middlewares/authMiddleware';
 
 const productRouter = express.Router();
 
-productRouter.get("/", getProducts);
-productRouter.get("/:id", paramValidator, validateId, getProduct);
+productRouter.get("/", protectedRouteMiddleware ,getProducts);
+productRouter.get("/:id", paramValidator, validateId, protectedRouteMiddleware, getProduct);
 
 export default productRouter;
