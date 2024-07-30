597ec7e47efb5f4222977913cfbc8a8533ad8b3b
earnstein <bdamilola00@gmail.com>
2024-07-30 12:19:12 +0100

    feat: added user router and auth router to approuter middleware

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import productRouter from "./products";
+import userRouter from "./users";
+import authRouter from "./auth";
 
 const appRouter = express.Router();
 
+appRouter.use("/auth", authRouter);
 appRouter.use("/product", productRouter);
+appRouter.use("/user", userRouter);
 
 export default appRouter;
