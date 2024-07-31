c36ec79e87a1f191d0fc3c20372d31853207eb3a
earnstein <bdamilola00@gmail.com>
2024-07-31 22:39:23 +0100

    chore: added product service functions

--- server/src/controllers/products.ts
+++ server/src/controllers/products.ts
@@ -1,8 +1,9 @@
 import type { Request, Response } from "express";
-import Product from "../models/productModel";
 import { StatusCodes } from "http-status-codes";
-import { NotFound } from "../utils/errors";
 import asyncHandler from "express-async-handler";
+import * as productService from "../services/productService";
+
+
 
 /**
  * @desc Fetch all products
@@ -11,14 +12,7 @@ import asyncHandler from "express-async-handler";
  */
 export const getProducts = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
-    const products = await Product.find(
-      {},
-      {
-        __v: 0,
-        updatedAt: 0,
-        createdAt: 0,
-      },
-    );
+    const products = await productService.getProducts();
     if (products.length === 0) {
       res.status(StatusCodes.OK).json({
         data: 0,
@@ -42,20 +36,7 @@ export const getProducts = asyncHandler(
 export const getProduct = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
-    const product = await Product.findById(
-      {
-        _id: id,
-      },
-      {
-        __v: 0,
-        updatedAt: 0,
-        createdAt: 0,
-      },
-    );
-
-    if (!product) {
-      throw new NotFound("product not found");
-    }
+    const product = await productService.getProduct(id);
     res.status(StatusCodes.OK).json(product);
     return;
   },
