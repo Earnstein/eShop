b295d52212eb3296268e6d403197f8f76886263c
earnstein <bdamilola00@gmail.com>
2024-08-01 22:49:32 +0100

    chore: formatted file

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,33 +1,42 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import ProductCard, { ProductProp } from "@/components/ProductCard";
-import { useGetProducts } from '../apis/api-hooks';
-import Loader from '@/components/Loader';
+import { useGetProducts } from "../apis/api-hooks";
+import Loader from "@/components/Loader";
 import { getProduct } from "@/apis/api";
 import { useQueryClient } from "@tanstack/react-query";
 import { STALETIME } from "@/constants";
 
 const HomePage = () => {
   const queryClient = useQueryClient();
-  const { data:products, isLoading, error, isError } = useGetProducts();
-  const prefetch = (id:string) => {
+  const { data: response, isLoading, error, isError } = useGetProducts();
+  const prefetch = (id: string) => {
     queryClient.prefetchQuery({
       queryKey: ["product", id],
-      queryFn:  () => getProduct(id),
+      queryFn: () => getProduct(id),
       staleTime: STALETIME,
-    })
+    });
   };
   return (
     <React.Fragment>
       <h1 className="text-center py-3">Latest Product</h1>
       <Row>
-        {isLoading && <Loader/>}
+        {isLoading && <Loader />}
         {isError && error?.message && <p>{error?.message}</p>}
-        {!isLoading && products.map((product: ProductProp) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3} onMouseEnter={()=>prefetch(product._id)} onFocus={()=>prefetch(product._id)}>
-            <ProductCard {...product} />
-          </Col>
-        ))}
+        {!isLoading &&
+          response?.data.map((product: ProductProp) => (
+            <Col
+              key={product._id}
+              sm={12}
+              md={6}
+              lg={4}
+              xl={3}
+              onMouseEnter={() => prefetch(product._id)}
+              onFocus={() => prefetch(product._id)}
+            >
+              <ProductCard {...product} />
+            </Col>
+          ))}
       </Row>
     </React.Fragment>
   );
