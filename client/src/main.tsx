07e373b4c239bb72004f959d6405b13f553c3768
earnstein <bdamilola00@gmail.com>
2024-08-01 22:48:02 +0100

    chore: added error page element / formatted file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import "@/assets/custom/bootstrap.custom.css";
-import { HomePage, ProductPage, CartPage } from "@/pages/index.ts"
+import { HomePage, ProductPage, CartPage, ErrorPage } from "@/pages/index.ts";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -11,31 +11,27 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
-
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
+    <Route path="/" element={<App />} errorElement={<ErrorPage />}>
       <Route path="/" element={<HomePage />} index={true} />
       <Route path="/product/:id" element={<ProductPage />} />
-      <Route path="/cart" element={<CartPage/>}/>
+      <Route path="/cart" element={<CartPage />} />
     </Route>
   )
-)
+);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <Toaster position='top-right' offset={70} duration={2000} richColors/>
+      <Toaster position="top-right" offset={70} duration={2000} richColors />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
