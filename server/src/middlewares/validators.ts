b049582bd3425324f5dc8fe3e8aa55d63542f2fd
earnstein <bdamilola00@gmail.com>
2024-08-01 12:50:44 +0100

    refactor: validateID and updateUserValidator middleware

--- server/src/middlewares/validators.ts
+++ server/src/middlewares/validators.ts
@@ -8,6 +8,7 @@ import {
   type ValidationChain,
 } from "express-validator";
 import { FAILED } from "../constants";
+import User from "../models/userModel";
 
 export const validate = (validations: ValidationChain[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -28,16 +29,15 @@ export const validate = (validations: ValidationChain[]) => {
   };
 };
 
-export const validateId = (req: Request, res: Response, next: NextFunction) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    throw new BadRequest(errors.array()[0].msg);
-  }
-  return next();
-};
-
-export const paramValidator = [
+export const validateId = [
   param("id").isMongoId().withMessage("Invalid ID"),
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new BadRequest(errors.array()[0].msg);
+    }
+    return next();
+  },
 ];
 
 export const signInValidator = [
@@ -52,4 +52,35 @@ export const signInValidator = [
 export const signUpValidator = [
   body("name").trim().notEmpty().withMessage("Name is required"),
   ...signInValidator,
+];
+
+export const updateUserValidator = [
+  body("name").trim().optional().notEmpty().withMessage("Name is required"),
+  body("email").isEmail().optional().withMessage("Email is required"),
+  body("password")
+    .trim()
+    .optional()
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name, email, password } = req.body;
+    if (!name && !email && !password) {
+      return next(
+        new BadRequest("At least one (name, email, password) is required")
+      );
+    };
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return next(new BadRequest("Email already in use"));
+    };
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(StatusCodes.UNPROCESSABLE_ENTITY)
+        .json({ message: errors.array(), status: FAILED, data: "" });
+    }
+    next();
+  },
 ];
