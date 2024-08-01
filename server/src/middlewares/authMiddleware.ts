78a6621d5a95a6ac2552aceb7f89c4bdcbf4db21
earnstein <bdamilola00@gmail.com>
2024-08-01 01:33:19 +0100

    feat: added validate Admin middleware/formatted file

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -1,35 +1,54 @@
-import jwt  from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 import type { Response, Request, NextFunction } from "express";
-import { UnAuthenticated } from "../utils/errors";
+import { UnAuthorized, Forbidden} from "../utils/errors";
 import User, { type I_UserDocument } from "../models/userModel";
-
+import jwt from "jsonwebtoken";
 
 interface CustomRequest extends Request {
   user?: I_UserDocument;
 }
 
 interface I_Payload {
-  _id : string;
+  _id: string;
 }
 
-
-export const protectedRouteMiddleware = asyncHandler(
-  async (req: CustomRequest, res: Response, next: NextFunction): Promise<void> => {
+export const protectedRouteMiddleware = asyncHandler(async (
+    req: CustomRequest,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
     const token: string = req.signedCookies[Bun.env.COOKIE_NAME!];
     if (!token || token.trim() === "") {
-      throw new UnAuthenticated("user not authenticated");
+      throw new UnAuthorized("user not authenticated");
     }
     try {
       const payload = <I_Payload>jwt.verify(token, Bun.env.JWT_SECRET!);
-      const user = await User.findById(payload._id)
-      if (!user){
-        throw new UnAuthenticated("User does not exist")
+      const user = await User.findById(
+        { _id: payload._id },
+        {
+          _id: 1,
+          isAdmin: 1,
+        }
+      );
+      if (!user) {
+        throw new UnAuthorized("User does not exist");
       }
       req.user = user;
       next();
     } catch (error) {
-      throw new UnAuthenticated("user token expired");
+      throw new UnAuthorized("user token expired");
     }
-  },
+  }
 );
+
+
+export const validateAdmin = (req: CustomRequest, res: Response,next: NextFunction) => {
+    if (!req.user) {
+      throw new UnAuthorized("user not authenticated");
+    }
+
+    if (!req.user.isAdmin){
+      throw new Forbidden("permission denied");
+    }
+    next()
+}
