ab4bbf7c2bbc9bb7553189ddb9a1eb5296c897c1
earnstein <bdamilola00@gmail.com>
2024-08-01 12:52:03 +0100

    feat: added get/update/delete user route

--- server/src/routers/users.ts
+++ server/src/routers/users.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { getUsers } from '../controllers/user';
+import { getUser, getUsers, updateUser, deleteUser } from '../controllers/user';
+import { protectedRouteMiddleware, validateAdmin, validateUser } from '../middlewares/authMiddleware';
+import { validateId, updateUserValidator } from '../middlewares/validators';
 
 const userRouter = express.Router();
 
 
-userRouter.get("/", getUsers);
+userRouter.get("/", protectedRouteMiddleware, validateAdmin, getUsers);
+userRouter.get("/:id", validateId, protectedRouteMiddleware, validateAdmin, getUser);
+userRouter.patch("/:id", validateId, updateUserValidator, protectedRouteMiddleware, validateUser, updateUser);
+userRouter.delete("/:id", validateId, protectedRouteMiddleware, validateUser, deleteUser);
 
 export default userRouter;
