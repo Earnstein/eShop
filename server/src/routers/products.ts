753abe4faa4ffd9d6f06139f799e9923c7e3e954
earnstein <bdamilola00@gmail.com>
2024-08-01 01:35:10 +0100

    chore: added validate admin middleware

--- server/src/routers/products.ts
+++ server/src/routers/products.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { getProducts, getProduct } from "../controllers/products";
 import { paramValidator, validateId } from "../middlewares/validators";
-import { protectedRouteMiddleware } from '../middlewares/authMiddleware';
+import { protectedRouteMiddleware, validateAdmin } from '../middlewares/authMiddleware';
 
 const productRouter = express.Router();
 
-productRouter.get("/", protectedRouteMiddleware ,getProducts);
-productRouter.get("/:id", paramValidator, validateId, protectedRouteMiddleware, getProduct);
+productRouter.get("/", protectedRouteMiddleware, validateAdmin, getProducts);
+productRouter.get("/:id", paramValidator, validateId, protectedRouteMiddleware, validateAdmin, getProduct);
 
 export default productRouter;
