907e73e4fa99d98a3305be5f9938ca5039cdc2d5
earnstein <bdamilola00@gmail.com>
2024-08-01 22:49:45 +0100

    add: error page export

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,10 +1,12 @@
 import HomePage from "@/pages/HomePage";
 import ProductPage from "@/pages/ProductPage";
 import CartPage from "@/pages/CartPage";
+import ErrorPage from "@/pages/ErrorPage"
 
 
 export {
     HomePage,
     ProductPage,
     CartPage,
+    ErrorPage
 }
