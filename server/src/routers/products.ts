f7b8a3986f41fd8f27d4715cde396d14dfcdb249
earnstein <bdamilola00@gmail.com>
2024-08-01 12:51:24 +0100

    chore: removed paramvalidator

--- server/src/routers/products.ts
+++ server/src/routers/products.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { getProducts, getProduct } from "../controllers/products";
-import { paramValidator, validateId } from "../middlewares/validators";
+import { validateId } from "../middlewares/validators";
 import { protectedRouteMiddleware, validateAdmin } from '../middlewares/authMiddleware';
 
 const productRouter = express.Router();
 
 productRouter.get("/", protectedRouteMiddleware, validateAdmin, getProducts);
-productRouter.get("/:id", paramValidator, validateId, protectedRouteMiddleware, validateAdmin, getProduct);
+productRouter.get("/:id", validateId, protectedRouteMiddleware, validateAdmin, getProduct);
 
 export default productRouter;
