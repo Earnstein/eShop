52cb7a50057c48d11a86c84f4f227899b1e9442a
earnstein <bdamilola00@gmail.com>
2024-08-01 22:52:16 +0100

    chore: removed protection middleware

--- server/src/routers/products.ts
+++ server/src/routers/products.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { getProducts, getProduct } from "../controllers/products";
 import { validateId } from "../middlewares/validators";
-import { protectedRouteMiddleware, validateAdmin } from '../middlewares/authMiddleware';
+import { protectedRouteMiddleware } from '../middlewares/authMiddleware';
 
 const productRouter = express.Router();
 
-productRouter.get("/", protectedRouteMiddleware, validateAdmin, getProducts);
-productRouter.get("/:id", validateId, protectedRouteMiddleware, validateAdmin, getProduct);
+productRouter.get("/", getProducts);
+productRouter.get("/:id", validateId, getProduct);
 
 export default productRouter;
