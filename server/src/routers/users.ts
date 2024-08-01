a528147709638655c302b43bf7ce30344d938af6
earnstein <bdamilola00@gmail.com>
2024-08-01 23:25:51 +0100

    add: added get current user route

--- server/src/routers/users.ts
+++ server/src/routers/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUser, getUsers, updateUser, deleteUser } from '../controllers/user';
+import { getUser, getUsers, updateUser, deleteUser, getCurrentUser } from '../controllers/user';
 import { protectedRouteMiddleware, validateAdmin, validateUser } from '../middlewares/authMiddleware';
 import { validateId, updateUserValidator } from '../middlewares/validators';
 
@@ -8,6 +8,7 @@ const userRouter = express.Router();
 
 userRouter.get("/", protectedRouteMiddleware, validateAdmin, getUsers);
 userRouter.get("/:id", validateId, protectedRouteMiddleware, validateAdmin, getUser);
+userRouter.get("/current/loggedin", protectedRouteMiddleware, getCurrentUser);
 userRouter.patch("/:id", validateId, updateUserValidator, protectedRouteMiddleware, validateUser, updateUser);
 userRouter.delete("/:id", validateId, protectedRouteMiddleware, validateUser, deleteUser);
 
