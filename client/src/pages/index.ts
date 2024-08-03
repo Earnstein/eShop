43a1a718fe384d10c46f2e951f34445e419f8461
earnstein <bdamilola00@gmail.com>
2024-08-03 23:36:41 +0100

    add: login page

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,12 +1,15 @@
 import HomePage from "@/pages/HomePage";
 import ProductPage from "@/pages/ProductPage";
 import CartPage from "@/pages/CartPage";
-import ErrorPage from "@/pages/ErrorPage"
+import ErrorPage from "@/pages/ErrorPage";
+import LoginPage from "@/pages/LoginPage"
+
 
 
 export {
     HomePage,
     ProductPage,
     CartPage,
-    ErrorPage
+    ErrorPage,
+    LoginPage
 }
