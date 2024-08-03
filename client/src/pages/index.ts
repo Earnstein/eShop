3d594bf0f67b2ca64d6156dacdaadb6dc0aa612a
earnstein <bdamilola00@gmail.com>
2024-08-03 23:41:52 +0100

    add: login and register page exports

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,7 +2,9 @@ import HomePage from "@/pages/HomePage";
 import ProductPage from "@/pages/ProductPage";
 import CartPage from "@/pages/CartPage";
 import ErrorPage from "@/pages/ErrorPage";
-import LoginPage from "@/pages/LoginPage"
+import LoginPage from "@/pages/LoginPage";
+import RegisterPage from "@/pages/RegisterPage";
+
 
 
 
@@ -11,5 +13,6 @@ export {
     ProductPage,
     CartPage,
     ErrorPage,
-    LoginPage
+    LoginPage,
+    RegisterPage
 }
