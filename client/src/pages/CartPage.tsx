406d017056292070944122cafe74aa73521bb5d3
earnstein <bdamilola00@gmail.com>
2024-08-04 02:09:25 +0100

    chore: reduced code by seperating logic to components

--- client/src/pages/CartPage.tsx
+++ client/src/pages/CartPage.tsx
@@ -1,100 +1,109 @@
-import { Link, useNavigate } from "react-router-dom";
-import {
-  Row,
-  Col,
-  ListGroup,
-  Image,
-  Button,
-  Card,
-  Alert,
-} from "react-bootstrap";
-import { FaTrash } from "react-icons/fa";
+// CartPage.js
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, ListGroup, Alert, Button } from "react-bootstrap";
+import { FaArrowLeft } from "react-icons/fa";
 import { toast } from "sonner";
-import useCartStore from "@/store/state";
+import useCartStore, { CartItem } from "@/store/state";
+import CartProduct from "@/components/CartItem";
+import PaginationControls from "@/components/PaginationControls";
+import CartSummary from "@/components/CartSummary";
 
-const CartPage = () => {
-  const navigate = useNavigate();
-  const { cartItems, removeFromCart, clearCartItems } = useCartStore();
+const ITEMS_PER_PAGE = 3;
+
+const CartPage: React.FC = () => {
+  const { cartItems, removeFromCart, clearCartItems, addToCart } =
+    useCartStore();
   const isEmpty = cartItems.length === 0;
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(cartItems.length / ITEMS_PER_PAGE);
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const currentItems = cartItems.slice(startIndex, endIndex);
+
+  useEffect(() => {
+    if (currentPage > totalPages) {
+      setCurrentPage(totalPages);
+    }
+  }, [cartItems, currentPage, totalPages]);
+
+  const handleUpdateCart = (item: CartItem, quantity: number) => {
+    const updatedItem: CartItem = { ...item, qty: quantity };
+    addToCart(updatedItem);
+    toast.info("Cart item updated");
+  };
+
+  const handleRemoveFromCart = (itemId: string) => {
+    removeFromCart(itemId);
+    toast.info("Item removed from cart");
+
+    if (currentItems.length === 1 && currentPage > 1) {
+      setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+    }
+  };
+
+  const handlePageChange = (pageNumber: number) => {
+    if (pageNumber > 0 && pageNumber <= totalPages) {
+      setCurrentPage(pageNumber);
+    }
+  };
 
   return (
     <Row className="mt-4">
       <Col md="8">
-        <h1 className="mb-4">Shopping Cart</h1>
+        <div className="d-flex gap-3 align-items-center mb-2">
+          <Link to="/">
+            <FaArrowLeft size={20} />
+          </Link>
+        </div>
         {isEmpty ? (
           <Alert variant="info">
             Your Shopping Cart is Empty <Link to={"/"}>Go back</Link>
           </Alert>
         ) : (
-          <ListGroup variant="flush">
-            {cartItems.map((item) => (
-              <ListGroup.Item key={item._id}>
-                <Row>
-                  <Col md={2}>
-                    <figure>
-                      <Image
-                        src={`/${item.image}`}
-                        alt={item.name}
-                        fluid
-                        rounded
-                      />
-                    </figure>
-                  </Col>
-                  <Col md={3}>
-                    <Link to={`/product/${item._id}`}>{item.name}</Link>
-                  </Col>
-                  <Col md={2}>$ {item.price}</Col>
-                  <Col>
-                    <span>{item.qty}</span>
-                  </Col>
-                  <Col md={2}>
-                    <Button
-                      variant="light"
-                      onClick={() => {
-                        removeFromCart(item._id!)
-                        toast.info("Item removed from cart")
-                      }}
-                    >
-                      <FaTrash />
-                    </Button>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
+          <>
+            <ListGroup variant="flush">
+              {/*CART COMPONENT*/}
+              {currentItems.map((item) => (
+                <CartProduct
+                  key={item._id}
+                  item={item}
+                  onRemove={handleRemoveFromCart}
+                  onUpdate={handleUpdateCart}
+                />
+              ))}
+            </ListGroup>
+
+            {/*PAGINATION COMPONENT*/}
+            {cartItems.length > ITEMS_PER_PAGE && (
+              <PaginationControls
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={handlePageChange}
+              />
+            )}
+            <Button
+              type="button"
+              className="btn-block"
+              onClick={() => {
+                clearCartItems();
+                toast("Cart Cleared");
+              }}
+            >
+              Clear Cart
+            </Button>
+          </>
         )}
-        <Button type="button" className="btn-block" onClick={()=>{
-          clearCartItems()
-          toast("Cart Cleared")
-          }}>
-          Clear Cart
-        </Button>
       </Col>
+      {/*CART SUMMARY COMPONENT*/}
       <Col md="4">
-        <Card>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h2>
-                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
-                items
-              </h2>
-              $
-              {cartItems
-                .reduce((acc, item) => acc + item.qty * item.price, 0)
-                .toFixed(2)}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Button
-                type="button"
-                className="btn-block"
-                disabled={cartItems.length === 0}
-                onClick={() => navigate("/login?redirect=/shipping")}
-              >
-                Proceed To Checkout
-              </Button>
-            </ListGroup.Item>
-          </ListGroup>
-        </Card>
+        <CartSummary
+          totalItems={cartItems.reduce((acc, item) => acc + item.qty, 0)}
+          totalAmount={cartItems.reduce((acc, item) => acc + item.qty * item.price, 0)}
+          isEmpty={isEmpty}
+        />
       </Col>
     </Row>
   );
