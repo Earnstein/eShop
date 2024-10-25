a9df75bf28054e3573bc89b396e25a8a5e9eae34
Bakare Damilola <bdamilola00@gmail.com>
2024-10-25 22:49:23 +0100

    feat: added login functionality

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,8 +1,12 @@
 import { Form, Button, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import FormContainter from "@/components/FormContainter";
 import { Formik, FormikHelpers } from "formik";
 import * as yup from "yup";
+import { useMutation } from "@tanstack/react-query";
+import { signIn } from "@/apis/api";
+import { toast } from "sonner";
+import useCartStore from "@/store/state";
 
 interface FormValues {
   email: string;
@@ -20,33 +24,46 @@ const initialValuesLogin: FormValues = {
 };
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const { addUser } = useCartStore();
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+
+  const { mutate, isSuccess, isPending } = useMutation({
+    mutationKey: ["login"],
+    mutationFn: (body: FormValues) => signIn(body),
+
+    onMutate: () => {
+      toast.loading("Signing in...");
+    },
+    onSuccess: (data) => {
+      addUser(data.data);
+      toast.dismiss();
+      toast.success("Sign in successful");
+      navigate(redirect);
+    },
+    onError: (error) => {
+      toast.error(error?.message);
+    },
+  });
   const handleFormSubmit = async (
     values: FormValues,
-    {resetForm}: FormikHelpers<FormValues>
+    { resetForm }: FormikHelpers<FormValues>
   ) => {
-    console.log(values);
-    resetForm();
+    mutate(values);
+    if (isSuccess) resetForm();
   };
   return (
     <FormContainter>
       <h1>Sign in</h1>
-      <Formik 
-      onSubmit={handleFormSubmit}
-      initialValues={initialValuesLogin}
-      validationSchema={loginSchema}
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValuesLogin}
+        validationSchema={loginSchema}
       >
-        {({
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit
-        })=>(
-          <Form
-          noValidate
-          
-          onSubmit={handleSubmit}
-          >
+        {({ errors, touched, handleBlur, handleChange, handleSubmit }) => (
+          <Form noValidate onSubmit={handleSubmit}>
             <Form.Group controlId="email" className="mb-4 rounded-0">
               <Form.Label className="title fw-bold">
                 Email <span className="text-danger star text-center">*</span>
@@ -83,17 +100,17 @@ const LoginPage = () => {
               </Form.Control.Feedback>
             </Form.Group>
 
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" disabled={isPending}>
               Submit
             </Button>
           </Form>
         )}
       </Formik>
       <Row className="py-3">
-          <Col>
-            Don't have an account? <Link to="/signup">Register</Link>
-          </Col>
-        </Row>
+        <Col>
+          Don't have an account? <Link to="/signup">Register</Link>
+        </Col>
+      </Row>
     </FormContainter>
   );
 };
