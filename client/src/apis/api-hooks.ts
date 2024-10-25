4c58f5d0383011be5cfda5b1239f3050e157b830
Bakare Damilola <bdamilola00@gmail.com>
2024-10-25 20:03:47 +0100

    chore: added comment for product api hooks

--- client/src/apis/api-hooks.ts
+++ client/src/apis/api-hooks.ts
@@ -1,28 +1,28 @@
-import {getProduct, getProducts } from "@/apis/api"
+import { getProduct, getProducts } from "@/apis/api";
 import { STALETIME } from "@/constants";
-import {  useQuery } from "@tanstack/react-query";
-import '@tanstack/react-query'
+import { useQuery } from "@tanstack/react-query";
+import "@tanstack/react-query";
 import { AxiosError } from "axios";
 
-declare module '@tanstack/react-query' {
+declare module "@tanstack/react-query" {
   interface Register {
-    defaultError: AxiosError
+    defaultError: AxiosError;
   }
 }
 
-
+//  PRODUCT APIS
 export const useGetProducts = () => {
-    return useQuery({
-        queryKey: ["products"],
-        queryFn: getProducts,
-        staleTime: STALETIME,
-    })
-}
+  return useQuery({
+    queryKey: ["products"],
+    queryFn: getProducts,
+    staleTime: STALETIME,
+  });
+};
 
 export const useGetProduct = (id: string) => {
-    return useQuery ({
-        queryKey: ["product", id],
-        queryFn: () => getProduct(id),
-        staleTime: STALETIME,
-    })
-}
+  return useQuery({
+    queryKey: ["product", id],
+    queryFn: () => getProduct(id),
+    staleTime: STALETIME,
+  });
+};
