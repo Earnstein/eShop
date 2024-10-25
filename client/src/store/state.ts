b97433020b19b190016d3174db6792fc7b6ecee2
Bakare Damilola <bdamilola00@gmail.com>
2024-10-25 22:50:30 +0100

    chore: added user state

--- client/src/store/state.ts
+++ client/src/store/state.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware";
+import { persist, createJSONStorage, devtools } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 export type CartItem = {
@@ -18,15 +18,20 @@ type ShippingAddress = {
   country: string;
 };
 
+type User = {
+  name: string;
+};
 type CartState = {
   cartItems: CartItem[];
   shippingAddress: ShippingAddress;
   paymentMethod: string;
   totalPrice: number;
+  user: User;
 };
 
 type Actions = {
   addToCart: (item: CartItem) => void;
+  addUser: (newUser: User) => void;
   removeFromCart: (id: string) => void;
   saveShippingAddress: (address: ShippingAddress) => void;
   savePaymentMethod: (method: string) => void;
@@ -39,6 +44,7 @@ const initialState = {
   shippingAddress: {} as ShippingAddress,
   paymentMethod: "PayPal",
   totalPrice: 0 as number,
+  user: {} as User,
 };
 
 const addDecimals = (num: number) => {
@@ -47,37 +53,25 @@ const addDecimals = (num: number) => {
 
 const useCartStore = create<CartState & Actions>()(
   persist(
-    immer((set) => ({
-      ...initialState,
-      addToCart: (newItem) =>
-        set((state) => {
-          const existingItem = state.cartItems.find(
-            (item) => item._id === newItem._id
-          );
-          if (existingItem) {
-            state.cartItems = state.cartItems.map((item) =>
-              item._id === existingItem._id ? newItem : item
+    devtools(
+      immer((set) => ({
+        ...initialState,
+        addUser: (newUser) =>
+          set((state) => {
+            state.user = newUser;
+          }),
+        addToCart: (newItem) =>
+          set((state) => {
+            const existingItem = state.cartItems.find(
+              (item) => item._id === newItem._id
             );
-          } else {
-            state.cartItems.push(newItem);
-          }
-          const itemsPrice = addDecimals(
-            state.cartItems.reduce(
-              (total, item) => total + item.price * item.qty,
-              0
-            )
-          );
-          const shippingPrice = addDecimals(itemsPrice > 1000 ? 0 : 10);
-          const tax = addDecimals(Number((0.15 * itemsPrice).toFixed(2)));
-          state.totalPrice = Number(itemsPrice + shippingPrice + tax);
-        }),
-      removeFromCart: (id) =>
-        set((state) => {
-          state.cartItems = state.cartItems.filter((x) => x._id !== id);
-          const isCartEmpty = state.cartItems.length === 0;
-          if (isCartEmpty) {
-            state.totalPrice = 0;
-          } else {
+            if (existingItem) {
+              state.cartItems = state.cartItems.map((item) =>
+                item._id === existingItem._id ? newItem : item
+              );
+            } else {
+              state.cartItems.push(newItem);
+            }
             const itemsPrice = addDecimals(
               state.cartItems.reduce(
                 (total, item) => total + item.price * item.qty,
@@ -87,23 +81,41 @@ const useCartStore = create<CartState & Actions>()(
             const shippingPrice = addDecimals(itemsPrice > 1000 ? 0 : 10);
             const tax = addDecimals(Number((0.15 * itemsPrice).toFixed(2)));
             state.totalPrice = Number(itemsPrice + shippingPrice + tax);
-          }
-        }),
-      saveShippingAddress: (address) =>
-        set((state) => {
-          state.shippingAddress = address;
-        }),
-      savePaymentMethod: (method) =>
-        set((state) => {
-          state.paymentMethod = method;
-        }),
-      clearCartItems: () =>
-        set((state) => {
-          state.cartItems = [];
-          state.totalPrice = 0;
-        }),
-      resetCart: () => set(() => initialState),
-    })),
+          }),
+        removeFromCart: (id) =>
+          set((state) => {
+            state.cartItems = state.cartItems.filter((x) => x._id !== id);
+            const isCartEmpty = state.cartItems.length === 0;
+            if (isCartEmpty) {
+              state.totalPrice = 0;
+            } else {
+              const itemsPrice = addDecimals(
+                state.cartItems.reduce(
+                  (total, item) => total + item.price * item.qty,
+                  0
+                )
+              );
+              const shippingPrice = addDecimals(itemsPrice > 1000 ? 0 : 10);
+              const tax = addDecimals(Number((0.15 * itemsPrice).toFixed(2)));
+              state.totalPrice = Number(itemsPrice + shippingPrice + tax);
+            }
+          }),
+        saveShippingAddress: (address) =>
+          set((state) => {
+            state.shippingAddress = address;
+          }),
+        savePaymentMethod: (method) =>
+          set((state) => {
+            state.paymentMethod = method;
+          }),
+        clearCartItems: () =>
+          set((state) => {
+            state.cartItems = [];
+            state.totalPrice = 0;
+          }),
+        resetCart: () => set(() => initialState),
+      }))
+    ),
     {
       name: "cart",
       storage: createJSONStorage(() => localStorage),
