7c8ed948876a927ada5503ccfaafe6e721b75e0b
Bakare Damilola <bdamilola00@gmail.com>
2024-10-26 01:37:31 +0100

    add: shipping import

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -4,15 +4,14 @@ import CartPage from "@/pages/CartPage";
 import ErrorPage from "@/pages/ErrorPage";
 import LoginPage from "@/pages/LoginPage";
 import RegisterPage from "@/pages/RegisterPage";
-
-
-
+import ShippingPage from "./ShippingPage";
 
 export {
-    HomePage,
-    ProductPage,
-    CartPage,
-    ErrorPage,
-    LoginPage,
-    RegisterPage
-}
+  HomePage,
+  ProductPage,
+  CartPage,
+  ErrorPage,
+  LoginPage,
+  RegisterPage,
+  ShippingPage,
+};
