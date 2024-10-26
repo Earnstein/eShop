5a955e9003b56858f1daff4916f828d64e160558
Bakare Damilola <bdamilola00@gmail.com>
2024-10-26 01:36:18 +0100

    chore: error component

--- client/src/components/ErrorComponent.tsx
+++ client/src/components/ErrorComponent.tsx
@@ -3,23 +3,24 @@ import { Button, Container, Row, Col } from "react-bootstrap";
 
 type ErrorProp = {
   message: string;
+  status: number;
 };
 
-const ErrorComponent = ({ message }: ErrorProp) => {
+const ErrorComponent: React.FC<ErrorProp> = ({ message, status }) => {
   return (
-    <Container> 
+    <Container>
       <Row>
         <Col className="text-center py-3">
-            <h1 className="display-1 fw-bold text-dark">404</h1>
-            <h2 className="fw-bold text-dark">Uh-oh!</h2>
-            <p>{message}</p>
-            <Link to="/">
-                <Button variant="primary">Go Back</Button>
-            </Link>
+          <h1 className="display-1 fw-bold text-dark">{status}</h1>
+          <h2 className="fw-bold text-dark">Uh-oh!</h2>
+          <p>{message}</p>
+          <Link to="/">
+            <Button variant="primary">Go Back</Button>
+          </Link>
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
 export default ErrorComponent;
