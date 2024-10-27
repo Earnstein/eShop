e64ff2be197a4eb8e6e2a5b6cc0f34ae3de9a359
Bakare Damilola <bdamilola00@gmail.com>
2024-10-27 01:09:59 +0100

    chore: added button to go back to previous page

--- client/src/components/ErrorComponent.tsx
+++ client/src/components/ErrorComponent.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { Button, Container, Row, Col } from "react-bootstrap";
 
 type ErrorProp = {
@@ -14,9 +13,9 @@ const ErrorComponent: React.FC<ErrorProp> = ({ message, status }) => {
           <h1 className="display-1 fw-bold text-dark">{status}</h1>
           <h2 className="fw-bold text-dark">Uh-oh!</h2>
           <p>{message}</p>
-          <Link to="/">
-            <Button variant="primary">Go Back</Button>
-          </Link>
+          <Button variant="primary" onClick={() => window.history.back()}>
+            Go Back
+          </Button>
         </Col>
       </Row>
     </Container>
