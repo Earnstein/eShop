76273e2f9175f93526970a208c40c56f06a90788
Bakare Damilola <bdamilola00@gmail.com>
2024-10-28 14:55:41 +0100

    chore: updated file with orderservice

--- server/src/controllers/orders.ts
+++ server/src/controllers/orders.ts
@@ -1,11 +1,10 @@
 import type { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import type { CustomRequest } from "../middlewares/authMiddleware";
-import Order, { type I_OrderDocument } from "../models/orderModel";
-import { BadRequest } from "../utils/errors";
-import Product from "../models/productModel";
+import { type I_OrderDocument } from "../models/orderModel";
 import { StatusCodes } from "http-status-codes";
 import { SUCCESS } from "../constants";
+import * as orderService from "../services/orderService";
 
 /**
  * @desc Create new order
@@ -14,55 +13,13 @@ import { SUCCESS } from "../constants";
  */
 export const createOrderHandler = asyncHandler(
   async (req: CustomRequest, res: Response): Promise<void> => {
-    const userId = req.user?._id;
-    const {
-      orderItems,
-      shippingAddress,
-      paymentMethod,
-      itemsPrice,
-      taxPrice,
-      shippingPrice,
-      totalPrice,
-    }: Omit<
+    const userId = req.user?._id as string;
+    const body: Omit<
       I_OrderDocument,
       "_id" | "user" | "isPaid" | "isDelivered" | "paidAt" | "deliveredAt"
     > = req.body;
 
-    if (!orderItems) {
-      throw new BadRequest("No order items");
-    }
-
-    const newOrderItems = await Promise.all(
-      orderItems.map(async (item) => {
-        const product = await Product.findById(item.product);
-        if (!product) {
-          throw new BadRequest("Product not found");
-        }
-        if (product.countInStock < Number(item.quantity)) {
-          throw new BadRequest(`${product.name} is out of stock`);
-        }
-        product.countInStock -= Number(item.quantity);
-        await product.save();
-        return {
-          name: item.name,
-          quantity: item.quantity,
-          image: item.image,
-          price: item.price,
-          product: product._id,
-        };
-      })
-    );
-    const order = new Order({
-      user: userId,
-      orderItems: newOrderItems,
-      shippingAddress,
-      paymentMethod,
-      itemsPrice,
-      taxPrice,
-      shippingPrice,
-      totalPrice,
-    });
-    const savedOrder = await order.save();
+    const savedOrder = await orderService.createOrder(userId, body);
 
     res.status(StatusCodes.CREATED).json({
       message: "Order created successfully",
@@ -80,7 +37,7 @@ export const createOrderHandler = asyncHandler(
  */
 export const getAllOrders = asyncHandler(
   async (req: CustomRequest, res: Response): Promise<void> => {
-    const orders = await Order.find({}).populate("user", "name");
+    const orders = await orderService.getAllOrders();
     res.status(StatusCodes.OK).json({
       message: "Orders fetched successfully",
       status: SUCCESS,
@@ -97,8 +54,8 @@ export const getAllOrders = asyncHandler(
  */
 export const getUserOrders = asyncHandler(
   async (req: CustomRequest, res: Response): Promise<void> => {
-    const userId = req.user?._id;
-    const orders = await Order.find({ user: userId });
+    const userId = req.user?._id as string;
+    const orders = await orderService.getUserOrders(userId);
     res.status(StatusCodes.OK).json({
       message: "Orders fetched successfully",
       status: SUCCESS,
@@ -116,11 +73,8 @@ export const getUserOrders = asyncHandler(
 
 export const getOrderById = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
-    const order = await Order.findById(id).populate("user", "name");
-    if (!order) {
-      throw new BadRequest("Order not found");
-    }
+    const { id: orderID } = req.params;
+    const order = await orderService.getOrderById(orderID);
     res.status(StatusCodes.OK).json({
       message: "Order fetched successfully",
       status: SUCCESS,
@@ -138,14 +92,8 @@ export const getOrderById = asyncHandler(
 
 export const updateOrderToPaid = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
-    const order = await Order.findById(id);
-    if (!order) {
-      throw new BadRequest("Order not found");
-    }
-    order.isPaid = true;
-    // order.paidAt = new Date();
-    const updatedOrder = await order.save();
+    const { id: orderID } = req.params;
+    const updatedOrder = await orderService.updateOrderToPaid(orderID);
     res.status(StatusCodes.OK).json({
       message: "Order updated successfully",
       status: SUCCESS,
