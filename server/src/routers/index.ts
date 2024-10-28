69d93362b6d1ddf4cfa921e1b523fd794a81ee9c
Bakare Damilola <bdamilola00@gmail.com>
2024-10-28 14:57:59 +0100

    add: order router

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -2,11 +2,12 @@ import express from "express";
 import productRouter from "./products";
 import userRouter from "./users";
 import authRouter from "./auth";
+import orderRouter from "./order";
 
 const appRouter = express.Router();
 
 appRouter.use("/auth", authRouter);
 appRouter.use("/product", productRouter);
 appRouter.use("/user", userRouter);
-
+appRouter.use("/order", orderRouter);
 export default appRouter;
