37d977e89a433b4f465cbf56c5af2c6bddc16e69
Bakare Damilola <bdamilola00@gmail.com>
2024-10-28 14:57:05 +0100

    feat: order router file

---
+++ server/src/routers/order.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import {
+  createOrderHandler,
+  getOrderById,
+  getAllOrders,
+  updateOrderToPaid,
+  getUserOrders,
+} from "../controllers/orders";
+import {
+  protectedRouteMiddleware,
+  validateAdmin,
+  validateUser,
+} from "../middlewares/authMiddleware";
+
+const orderRouter = express.Router();
+
+orderRouter.post("/", protectedRouteMiddleware, createOrderHandler);
+orderRouter.get("/", protectedRouteMiddleware, validateAdmin, getAllOrders);
+orderRouter.get(
+  "/myorders",
+  protectedRouteMiddleware,
+  validateUser,
+  getUserOrders
+);
+orderRouter.get("/:id", protectedRouteMiddleware, validateAdmin, getOrderById);
+orderRouter.patch(
+  "/:id",
+  protectedRouteMiddleware,
+  validateAdmin,
+  updateOrderToPaid
+);
+export default orderRouter;
