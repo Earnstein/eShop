e9fcda24c5abbeadd10f1d67c6829eb7b963f731
Bakare Damilola <bdamilola00@gmail.com>
2024-10-29 01:11:33 +0100

    chore: formatted file

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -1,20 +1,26 @@
 import { useGetProduct } from "@/apis/api-hooks";
 import Rating from "@/components/Rating";
 import React from "react";
-import { Button, Card, Col, Image, ListGroup, Row, Form } from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
-import Loader from '@/components/Loader';
-import useCartStore, { CartItem } from '@/store/state';
+import {
+  Button,
+  Card,
+  Col,
+  Image,
+  ListGroup,
+  Row,
+  Form,
+} from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import Loader from "@/components/Loader";
+import useCartStore, { CartItem } from "@/store/state";
 import { toast } from "sonner";
 import { FaArrowLeft } from "react-icons/fa";
 
-
-
 const ProductPage = () => {
-  const [ qty , setQty ] = React.useState(1);
+  const [qty, setQty] = React.useState(1);
   const { id } = useParams();
   const { addToCart } = useCartStore();
-  const { data:response, isPending, isError, error } = useGetProduct(id!);
+  const { data: response, isPending, isError, error } = useGetProduct(id!);
   const handleCart = () => {
     const newProduct: CartItem = {
       _id: response.data._id,
@@ -23,67 +29,80 @@ const ProductPage = () => {
       price: response.data.price,
       image: response.data.image,
       countInStock: response.data.countInStock,
-    }
+    };
     addToCart(newProduct);
-    toast.success("Item added to cart")
-  }
+    toast.success("Item added to cart");
+  };
 
   return (
     <React.Fragment>
-      <Link to="/">
-        <div className="my-3">
-          <FaArrowLeft size={20} />
-        </div>
-      </Link>
-      {isPending && <Loader/>}
-      {isError && error?.message && <p>{error?.message} {error?.response?.status}</p>}
-        {response && (
-         <Row>
-           <Col md={5}>
-          <figure>
-            <Image src={`/${response?.data.image}`} alt={response?.data.name} fluid />
-          </figure>
-        </Col>
-        <Col md={4}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{response?.data.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              {response && (
-                <Rating
-                  rating={response?.data.rating}
-                  numReviews={response?.data.numReviews}
-                />
-              )}
-            </ListGroup.Item>
-            <ListGroup.Item><span className="fw-bold">Description:</span> {response?.data.description}</ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+      <FaArrowLeft
+        className="my-3"
+        size={20}
+        onClick={() => window.history.back()}
+      />
+
+      {isPending && <Loader />}
+      {isError && error?.message && (
+        <p>
+          {error?.response?.data?.message || error?.message}{" "}
+          {error?.response?.status}
+        </p>
+      )}
+      {response && (
+        <Row>
+          <Col md={5}>
+            <figure>
+              <Image
+                src={`/${response?.data.image}`}
+                alt={response?.data.name}
+                fluid
+              />
+            </figure>
+          </Col>
+          <Col md={4}>
             <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${response?.data.price}</strong>
-                  </Col>
-                </Row>
+                <h3>{response?.data.name}</h3>
               </ListGroup.Item>
-
               <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    <strong>
-                      {response && response?.data.countInStock > 0
-                        ? "In Stock"
-                        : "Out of Stock"}
-                    </strong>
-                  </Col>
-                </Row>
+                {response && (
+                  <Rating
+                    rating={response?.data.rating}
+                    numReviews={response?.data.numReviews}
+                  />
+                )}
               </ListGroup.Item>
+              <ListGroup.Item>
+                <span className="fw-bold">Description:</span>{" "}
+                {response?.data.description}
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${response?.data.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      <strong>
+                        {response && response?.data.countInStock > 0
+                          ? "In Stock"
+                          : "Out of Stock"}
+                      </strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
                 {response && response?.data.countInStock > 0 && (
                   <ListGroup.Item>
                     <Row>
@@ -94,31 +113,33 @@ const ProductPage = () => {
                           value={qty}
                           onChange={(e) => setQty(Number(e.target.value))}
                         >
-                          {[...Array(response?.data.countInStock).keys()].map((x) => (
-                            <option key={x + 1} value={x + 1}>
-                              {x + 1}
-                            </option>
-                          ))}
+                          {[...Array(response?.data.countInStock).keys()].map(
+                            (x) => (
+                              <option key={x + 1} value={x + 1}>
+                                {x + 1}
+                              </option>
+                            )
+                          )}
                         </Form.Select>
                       </Col>
                     </Row>
                   </ListGroup.Item>
                 )}
-              <ListGroup.Item>
-                <Button
-                  className="btn-block"
-                  type="button"
-                  disabled={response && response?.data.countInStock === 0}
-                  onClick={handleCart}
-                >
-                  Add To Cart
-                </Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-         </Row>
-        )}
+                <ListGroup.Item>
+                  <Button
+                    className="btn-block"
+                    type="button"
+                    disabled={response && response?.data.countInStock === 0}
+                    onClick={handleCart}
+                  >
+                    Add To Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </React.Fragment>
   );
 };
