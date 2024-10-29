bb245e9f3078cd728f5d9e47523fcca3181fe52b
Bakare Damilola <bdamilola00@gmail.com>
2024-10-29 22:31:52 +0100

    chore: added effect for getting logged in user

--- client/src/pages/ProtectedRoute.tsx
+++ client/src/pages/ProtectedRoute.tsx
@@ -1,8 +1,20 @@
 import useAuthStore from "@/store/userState";
 import { Outlet, Navigate } from "react-router-dom";
+import { toast } from "sonner";
+import { useGetCurrentUser } from "@/apis/api-hooks";
+import { useEffect } from "react";
 
 const ProtectedRoute = () => {
-  const { isLoggedIn } = useAuthStore();
+  const { isLoggedIn, clearUser, setIsLoggedIn } = useAuthStore();
+  const { data: currentUser } = useGetCurrentUser();
+
+  useEffect(() => {
+    if (!currentUser) {
+      toast.info("Session expired, please login again");
+      clearUser();
+    }
+    setIsLoggedIn(true);
+  }, [currentUser, clearUser]);
 
   return isLoggedIn ? <Outlet /> : <Navigate to="/signin" replace />;
 };
