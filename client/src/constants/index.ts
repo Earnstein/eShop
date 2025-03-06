b8028f9ee153f6fa1e132e7f2a20b1ddf5e78659
Bakare Damilola <bdamilola00@gmail.com>
2025-03-07 00:18:49 +0100

    chore: change base url to match server

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:8000/v1";
+export const BASE_URL = "http://localhost:8000/api";
 export const STALETIME = 60000;
+export const PAYPAL_URL = "config/paypal";
