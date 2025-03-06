3bfbac3ba518ba14e349a4ea5a49a8eb0d82fcdb
Bakare Damilola <bdamilola00@gmail.com>
2025-03-07 00:19:57 +0100

    add: paypal config route

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -3,9 +3,19 @@ import productRouter from "./products";
 import userRouter from "./users";
 import authRouter from "./auth";
 import orderRouter from "./order";
-
+import { SUCCESS } from "../constants";
+import Bun from "bun";
 const appRouter = express.Router();
 
+appRouter.get("/config/paypal", (req, res) => {
+  //   res.send({ clientId: Bun.env.PAYPAL_CLIENT_ID! });
+  res.status(200).json({
+    data: Bun.env.PAYPAL_CLIENT_ID,
+    status: SUCCESS,
+    messsage: "",
+  });
+  return;
+});
 appRouter.use("/auth", authRouter);
 appRouter.use("/product", productRouter);
 appRouter.use("/user", userRouter);
